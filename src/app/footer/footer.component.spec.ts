45f501c970a665d8f00efc17fd4d06f05b564b38
Cristhian David Patarroyo Cuellar <50779675+cristhian1820@users.noreply.github.com>
2021-09-17 23:17:17 -0500

    primeros pasos

---
+++ src/app/footer/footer.component.spec.ts
@@ -0,0 +1,7 @@
+import { FooterComponent } from './footer.component';
+
+describe('Footer.Component.Ts', () => {
+  it('should create an instance', () => {
+    expect(new FooterComponent()).toBeTruthy();
+  });
+});
