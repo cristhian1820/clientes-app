2c618bf8d6b4d1202da9cd2e66102dd31338ba85
Cristhian David Patarroyo Cuellar <50779675+cristhian1820@users.noreply.github.com>
2021-10-08 23:36:28 -0500

    avances

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,14 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  { id: 1, nombre: 'Cristhian', apellido: 'Patarroyo', email: 'cristhian@gmail.com', createAt: '2021-09-20' },
+  { id: 2, nombre: 'Jose', apellido: 'Venitez', email: 'Jose@gmail.com', createAt: '2021-09-20' },
+  { id: 3, nombre: 'Andres', apellido: 'Gonzales', email: 'Andres@gmail.com', createAt: '2021-09-20' },
+  { id: 4, nombre: 'Carlos', apellido: 'Cuellar', email: 'Carlos@gmail.com', createAt: '2021-09-20' },
+  { id: 5, nombre: 'Eliana', apellido: 'Avila', email: 'Eliana@gmail.com', createAt: '2021-09-20' },
+  { id: 6, nombre: 'Maria', apellido: 'Cortez', email: 'Maria@gmail.com', createAt: '2021-09-20' },
+  { id: 7, nombre: 'Mario', apellido: 'Barrera', email: 'Mario@gmail.com', createAt: '2021-09-20' },
+  { id: 8, nombre: 'Jhon', apellido: 'Cadabi', email: 'Jhon@gmail.com', createAt: '2021-09-20' },
+  { id: 9, nombre: 'Laura', apellido: 'Ordoñez', email: 'Laura@gmail.com', createAt: '2021-09-20' },
+  { id: 10, nombre: 'Melissa', apellido: 'Gomez', email: 'Melissa@gmail.com', createAt: '2021-09-20' }
+];
