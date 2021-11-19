81f73564b693ffcc639a780190b10d058789ccf0
Cristhian David Patarroyo Cuellar <50779675+cristhian1820@users.noreply.github.com>
2021-11-19 12:02:16 -0500

    primeros pasos

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-directiva',
-  templateUrl: './directiva.component.html',
-  styleUrls: ['./directiva.component.css']
+  templateUrl: './directiva.component.html'
+
 })
 export class DirectivaComponent {
 
